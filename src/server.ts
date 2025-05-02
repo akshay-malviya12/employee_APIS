6291127deca2d758244e518be4f466dbcc695396
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2025-05-02 22:08:53 +0530

    Update server.ts
    add server code here.. ..

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import sequelize from './config/db';
+import router from './router/router';
+
+//env configuration ..
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript with Node.js!');
+});
+
+app.use(router);
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port :- ${PORT}`)
+    })
+})
+
+
+
+
 
