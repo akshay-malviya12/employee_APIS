62c0c15287c767a10b4bda52404815573c178c14
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2025-05-02 22:07:29 +0530

    Update empTables.ts
    add employee table schema ..

--- src/models/empTables.ts
+++ src/models/empTables.ts
@@ -1,1 +1,81 @@
 
+
+import { Sequelize, DataTypes, INTEGER } from "sequelize";
+import sequelize from "../config/db";
+import User from "./usertable";
+
+//create table structure for employees..
+const Employee = sequelize.define("Employee", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  basic_salary: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  HRA: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  allowances: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  tax: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  deductions: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  PF: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  attendance: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('Employee', 'HR', 'Admin'),
+    allowNull: false
+  },
+  workingHours: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  month: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "User", // table name
+      key: "id"
+    }
+  },
+});
+
+// Set up the One-to-Many relationship
+User.hasMany(Employee, { foreignKey: 'userId' }); // User1 can have many User2
+
+sequelize.sync().then(() => {
+  console.log('employee table created successfully!');
+}).catch((error) => {
+  console.error('Unable to create table : ', error);
+});
+
+export default Employee;
+
+
+
+
+
+
+
