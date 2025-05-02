06b3cf18ac54303ce38efee7ac421ab13f1c3118
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2025-05-02 22:05:27 +0530

    Update db.ts
    add db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,30 @@
+import { Sequelize } from 'sequelize';
+
+import dotenv from "dotenv";
+
+//dot env configuration.
+dotenv.config();
+
+const PASSWORD = process.env.DB_PASSWORD as string;
+const DBNAME = process.env.DB_NAME as string;
+
+const sequelize = new Sequelize(
+  DBNAME,
+  'root',
+  PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: Number(process.env.DB_PORT),
+    logging: false,
+  }
+)
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
+export default sequelize;
 
