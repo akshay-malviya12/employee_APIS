c3c19e8a19349ab8dc1f79271b570b33d3cac6d7
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2025-05-02 22:06:08 +0530

    Update attendance.ts
    add attendance

--- src/controller/attendance.ts
+++ src/controller/attendance.ts
@@ -1,1 +1,28 @@
 
+import express, { Request, Response } from "express";
+import Employee from "../models/empTable";
+
+const router = express.Router();
+
+//employee attendance
+export const attendance = async (req: Request, res: Response) => {
+    const { id, attendance } = req.body
+    if (!id) {
+        res.status(400).json({ "error": "employee id not found." })
+    }
+
+    try {
+        const attendanceEmp = await Employee.update(
+            { attendance: String(attendance) },
+            { where: { id } }
+        );
+        
+        res
+            .status(200)
+            .json({ message: "Employee attendance added" });
+    } catch (error) {
+        res.status(500).json({ "message": error })
+    }
+
+}
+
